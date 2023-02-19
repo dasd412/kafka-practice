3952a006dfb811b7faad9bbfdaf3ef0c6543970f
dasd <dasd412@naver.com>
2023-02-19 20:32:47 +0900

    Change sync commit of consumer

--- kafka-consumer/src/main/java/org/example/SimpleConsumer.java
+++ kafka-consumer/src/main/java/org/example/SimpleConsumer.java
@@ -28,6 +28,7 @@ public class SimpleConsumer {
         configs.put(ConsumerConfig.GROUP_ID_CONFIG,GROUP_ID);
         configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class.getName());
+        configs.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG,false);
 
         KafkaConsumer<String,String> consumer= new KafkaConsumer<>(configs);
         consumer.subscribe(Arrays.asList(TOPIC_NAME));
@@ -36,6 +37,7 @@ public class SimpleConsumer {
             ConsumerRecords<String,String>records=consumer.poll(Duration.ofSeconds(1));
             for(ConsumerRecord<String,String>record:records){
                 logger.info("{}",record);
+                consumer.commitAsync();
             }
         }
     }
